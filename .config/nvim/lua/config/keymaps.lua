0033c99ee22eb87638202a2a1c63b6712f27fb54
jwilson232 <joseph.wilson@thedatarefinery.co.uk>
2025-08-12 13:09:41 +0100

    Adds telescope to nvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,2 +1,6 @@
 vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", {desc="Open parent directory in old"})
 vim.keymap.set("n", "<Tab>", "<C-w><C-w>", {desc="Moves to next split buffer with tab"})
+vim.keymap.set("n", "t", "<cmd> vs term://zsh<CR>", {desc="Moves to next split buffer with tab"})
+vim.keymap.set("t", "<ESC>", "<C-\\><C-n>", {noremap = true})
+vim.keymap.set('n', '<Space>', '<Nop>')
+
